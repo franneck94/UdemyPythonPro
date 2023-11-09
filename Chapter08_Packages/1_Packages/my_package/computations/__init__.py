dbc595cf5e49169ec2b8ec6d46eaeae67edc5ac4
franneck94 <schaffranek.jan@web.de>
2023-11-09 22:09:02 +0100

    up

---
+++ Chapter08_Packages/1_Packages/my_package/computations/__init__.py
@@ -0,0 +1,2 @@
+from .intern import addition  # noqa: F401
+from .intern import subtraction  # noqa: F401
---
+++ Chapter09_CPython/cython/math_cython/__init__.py
@@ -0,0 +1,5 @@
+from .computations import cython_clip_vector  # noqa: F401
+from .computations import cython_np_clip_vector  # noqa: F401
+from .computations import naive_cython_clip_vector  # noqa: F401
+from .computations import parallel_cython_clip_vector  # noqa: F401
+from .computations import python_clip_vector  # noqa: F401
