3cec1b1e05f816fda93359741a5673df9ca730e4
franneck94 <schaffranek.jan@web.de>
2023-11-13 16:15:01 +0100

    up

--- Chapter06_CodingGuidelines/TypeAnnotations/callable.py
+++ Chapter06_CodingGuidelines/TypeAnnotations/callable.py
@@ -16,29 +16,9 @@ def function(
     print_fn(values)
 
 
-def append_value(
-    value: int,
-    my_list: list[int] | None = None,
-) -> list[int]:
-    if my_list:
-        my_list.append(value)
-    else:
-        my_list = [value]
-    return my_list
-
-
 def main() -> None:
     values = [1, 2, 3]
-    # expand_ratio = 2
-
     function(values, print_list)
-
-    my_list = append_value(1)
-    print(my_list)
-    my_list = append_value(2, my_list)
-    print(my_list)
-    my_list = append_value(3, my_list)
-    print(my_list)
 
 
 if __name__ == "__main__":
